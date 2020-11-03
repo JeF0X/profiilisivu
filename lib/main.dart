710e86f4c2545d76a5686df264a287afa3676463
JeF0X <jekettu@gmail.com>
2020-11-03 12:03:54 +0200

    Valmis

--- lib/main.dart
+++ lib/main.dart
@@ -23,43 +23,70 @@ class ProfilePage extends StatelessWidget {
                 color: Colors.white,
                 fontSize: 30.0,
                 fontFamily: 'Kaushan Script',
+                fontWeight: FontWeight.bold,
               ),
             ),
             Text(
-              'Best cat in the world',
+              'Best cat in the world'.toUpperCase(),
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 20.0,
                 fontFamily: 'Source Sans Pro',
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
               ),
             ),
-            ListTile(
-              leading: Icon(
-                Icons.phone,
-                color: Colors.white,
-                size: 40.0,
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                thickness: 1,
+                color: Colors.deepOrange,
               ),
-              title: Text(
-                '040 8544833',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                  fontFamily: 'Source Sans Pro',
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.deepOrange,
+                    size: 40.0,
+                  ),
+                  title: Text(
+                    '040 8544833',
+                    style: TextStyle(
+                      color: Colors.deepOrange,
+                      fontSize: 20.0,
+                      fontFamily: 'Source Sans Pro',
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.0,
+                    ),
+                  ),
                 ),
               ),
             ),
-            ListTile(
-              leading: Icon(
-                Icons.email,
-                color: Colors.white,
-                size: 40.0,
-              ),
-              title: Text(
-                'viljami@email.fi',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                  fontFamily: 'Source Sans Pro',
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.deepOrange,
+                    size: 40.0,
+                  ),
+                  title: Text(
+                    'viljami@email.fi',
+                    style: TextStyle(
+                      color: Colors.deepOrange,
+                      fontSize: 20.0,
+                      fontFamily: 'Source Sans Pro',
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.0,
+                    ),
+                  ),
                 ),
               ),
             ),
